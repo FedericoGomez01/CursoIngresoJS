40271ff719f869dfdf3898d0428becd352f4ac2a
unknown <fedegomez.pg@gmail.com>
2021-02-02 19:12:11 -0300

    Ejercicio 3 resuelto

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,12 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	var nombreDelAlumno;
+	var edadDelAlumno;
+
+	nombreDelAlumno= document.getElementById('txtIdNombre').value;
+	edadDelAlumno= prompt("Ingrese su edad", "Ej.:12 años");
+
+	document.write("Usted se llama "+ nombreDelAlumno + " y tiene "+ edadDelAlumno);
 
 }
-
-
