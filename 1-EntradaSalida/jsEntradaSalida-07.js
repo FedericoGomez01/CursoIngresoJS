6ab059a259f11fcbf062f2d9a4b978153e7b9612
unknown <fedegomez.pg@gmail.com>
2021-02-02 20:44:15 -0300

    Ejercicio 7 resuelto

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,25 +1,72 @@
 /*
+Gomez Federico 1H
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
-}
+	var numUno;
+	var numDos;
+	var sumar;
 
+	numUno = document.getElementById('txtIdNumeroUno').value;
+	numDos = document.getElementById('txtIdNumeroDos').value;
+
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+
+	sumar = numUno + numDos;
+
+	alert("El resultado de la suma de sus numeros es: " + sumar);
+}
 function restar()
 {
-	
+	var numUno;
+	var numDos;
+	var restar;
+
+	numUno = document.getElementById('txtIdNumeroUno').value;
+	numDos = document.getElementById('txtIdNumeroDos').value;
+
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+
+	restar = numUno - numDos;
+
+	alert("El resultado de la suma de sus numeros es: " + restar);
 }
 
 function multiplicar()
 { 
-	
+	var numUno;
+	var numDos;
+	var multiplicar;
+
+	numUno = document.getElementById('txtIdNumeroUno').value;
+	numDos = document.getElementById('txtIdNumeroDos').value;
+
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+
+	multiplicar = numUno * numDos;
+
+	alert("El resultado de la suma de sus numeros es: " + multiplicar);
 }
 
 function dividir()
 {
-	
-}
+	var numUno;
+	var numDos;
+	var division;
 
+	numUno = document.getElementById('txtIdNumeroUno').value;
+	numDos = document.getElementById('txtIdNumeroDos').value;
+
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+
+	division = numUno / numDos;
+
+	alert("El resultado de la suma de sus numeros es: " + division);
+}
