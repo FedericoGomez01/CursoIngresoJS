c021a68f05d5d09312814e0b631603bee7cc7158
unknown <fedegomez.pg@gmail.com>
2021-02-02 19:14:40 -0300

    Ejercicio 1 resuelto

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,14 @@
 /*
+Gomez Federico 
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+/*
+Ejercicio 1
+*/
+
+
 function mostrar()
 {
-	alert("Hola mundo");
+	alert("Esto funciona de maravilla");
 }
 
