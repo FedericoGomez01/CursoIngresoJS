147007d8f4c9eed00fd36b5f77896ad9940fb3ef
unknown <fedegomez.pg@gmail.com>
2021-02-02 20:12:08 -0300

    Ejercicio 6 resuelto

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,27 @@
 /*
+Gomez Federico 1H
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numUno;
+	var numDos;
+	var suma;
+
+	//Entradas
+	numUno = txtIdNumeroUno.value;
+	numDos = txtIdNumeroDos.value;
+	
+	//Conversiones (Parseo)
+	/* El parseInt() es una funcion que toma el 
+	dato almacenado y lo convierte en numerico*/
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+
+
+	suma = numUno + numDos;
+
+	alert("La suma de los dos numeros es: "+ suma);
 }
 
