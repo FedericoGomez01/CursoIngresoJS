7cd7f930f9dbed3f700875d556b43f47ceaab430
unknown <fedegomez.pg@gmail.com>
2021-02-03 17:29:28 -0300

    TP 8 realizado

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,79 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var numUno;
+	var numDos;
+	var numTres;
+	var suma;
+
+
+	//entrada de los datos.
+	numUno = document.getElementById('txtIdPrecioUno').value;
+	numDos = document.getElementById('txtIdPrecioDos').value;
+	numTres = document.getElementById('txtIdPrecioTres').value;
+
+	//parseo de datos.
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	numTres = parseInt(numTres);
+
+	//ecuacion 
+	suma = numUno + numDos + numTres;
+
+	//salida de datos procesados.
+	alert("La suma de los 3 productos es: " + suma);
+
 }
 function Promedio () 
 {
-	
+	var numUno;
+	var numDos;
+	var numTres;
+	var promedio;
+
+	//entrada de los datos
+	numUno = document.getElementById('txtIdPrecioUno').value;
+	numDos = document.getElementById('txtIdPrecioDos').value;
+	numTres = document.getElementById('txtIdPrecioTres').value;
+
+	//parsear de datos.
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	numTres = parseInt(numTres);
+
+	//ecuacion de los datos.
+	promedio = (numUno + numDos + numTres) / 3;
+
+	//toFixed para achicar el numero float.
+	promedio = promedio.toFixed(2);
+
+	//salida de los datos procesados.
+	alert("El promedio del precio de los tres productos es de: " + promedio);
 }
 function PrecioFinal () 
 {
-	
+	var numUno;
+	var numDos;
+	var numTres;
+	var final;
+	var aumento;
+
+	//entrada de los datos
+	numUno = document.getElementById('txtIdPrecioUno').value;
+	numDos = document.getElementById('txtIdPrecioDos').value;
+	numTres = document.getElementById('txtIdPrecioTres').value;
+
+	//parsear de datos.
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	numTres = parseInt(numTres);
+
+	//ecuacionde los datos (sumar un 21%)
+	aumento = 0.21;
+	final = (numUno + numDos + numTres) * aumento;
+
+	//salida de los datos procesados.
+	alert("El precio final de los tres productos mas el 'IVA' es: " + final);
+
+
 }
