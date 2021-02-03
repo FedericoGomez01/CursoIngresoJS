344d513138706bd6e9d916cfd624a6d496414291
unknown <fedegomez.pg@gmail.com>
2021-02-02 22:30:31 -0300

    Ejercicio 9 y 10 resueltos

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,22 @@
 /*
+Gomez Federico 1H
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
+	var aumento;
 
-	alert("ok");
+	sueldo = document.getElementById('txtIdSueldo').value;
+	sueldo = parseInt(sueldo);
+
+	aumento = sueldo * 0.10
+	resultado = aumento + sueldo;
+
+
+	document.getElementById('txtIdResultado').value = resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,25 @@
 /*
+Gomez Federico 1H
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe;
+	var decremento;
+	var resultado;
+
+	importe = document.getElementById('txtIdImporte').value;
+	importe = parseInt(importe);
+
+
+	//decremento = importe * 0.25;
+	//resultado = importe - decremento;
+
+	//resultado = importe - (importe * 0.25);
+
+	resultado = importe * 0.75
+
+	document.getElementById('txtIdResultado').value = resultado;
 }
