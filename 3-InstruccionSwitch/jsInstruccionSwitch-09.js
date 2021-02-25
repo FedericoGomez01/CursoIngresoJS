c57a68294bac62b2ed59b9ff985b970c8a7211cd
unknown <fedegomez.pg@gmail.com>
2021-02-25 09:32:27 -0300

    Ejercicio 9 de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,26 +1,6 @@
 /*
 
-Gomez Federico 1H
 
-una agencia de viajes debe sacar las tarifas de los viajes,
-se cobra $15.000 por cada estadia como base, se 
-pide el ingreso de una estacion del año y localidad
-para vacacionar para poder calcular el precio final
-
-en Invierno: bariloche tiene un aumento del 20% cataratas
-y Cordoba tiene un descuento del 10% Mar del plata tiene
-un descuento del 20%
-
-en Verano: bariloche tiene un descuento del 20% cataratas
-y Cordoba tiene un aumento del 10% Mar del plata tiene
-un aumento del 20%
-
-en Otoño y Primavera: bariloche tiene un aumento
-del 10% cataratas tiene un aumento del 10% Mar del
-plata tiene un aumento del 10% y Cordoba tiene el 
-precio sin descuento
-
-*/
 
 
 function mostrar()
@@ -96,10 +76,94 @@ function mostrar()
 	{
 		precioFinal = tarifa - (tarifa*descuento);
 	}
-	// precioFinal = tarifa - (tarifa*descuento);
-	// precioFinal = tarifa + (tarifa*aumento);
+
 
 	console.log("precio final: " + precioFinal + "aumento: " + aumento + "descuento: " + descuento);
 	alert(precioFinal);
 
+}
+Gomez Federico 1H
+
+una agencia de viajes debe sacar las tarifas de los viajes,
+se cobra $15.000 por cada estadia como base, se 
+pide el ingreso de una estacion del año y localidad
+para vacacionar para poder calcular el precio final
+
+en Invierno: bariloche tiene un aumento del 20% cataratas
+y Cordoba tiene un descuento del 10% Mar del plata tiene
+un descuento del 20%
+
+en Verano: bariloche tiene un descuento del 20% cataratas
+y Cordoba tiene un aumento del 10% Mar del plata tiene
+un aumento del 20%
+
+en Otoño y Primavera: bariloche tiene un aumento
+del 10% cataratas tiene un aumento del 10% Mar del
+plata tiene un aumento del 10% y Cordoba tiene el 
+precio sin descuento
+*/
+
+function mostrar()
+{
+	var tarifaViajes;
+	var estadiaBase;
+	var estacionDelAño;
+	var localidad;
+	var aumento;
+
+	estadiaBase = 15000;
+
+	estacionDelAño = document.getElementById('txtIdEstacion').value;
+	localidad = document.getElementById('txtIdDestino').value;
+
+
+	switch (estacionDelAño) 
+	{
+		case "Invierno":
+			switch (localidad) 
+			{
+				case "Bariloche":
+					aumento = 1.20;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					aumento = 0.90;
+					break;
+				case "Mar del Plata":
+					aumento = 0.80;
+					break;
+			}
+			break;
+		case "Verano":
+			switch (localidad) 
+			{
+				case "Bariloche":
+					aumento = 0.80;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					aumento = 1.10;
+					break;
+				case "Mar del Plata":
+					aumento = 1.20;
+					break;
+			}
+			break;
+		default:
+			switch (localidad) 
+			{
+				case "Bariloche":
+				case "Mar del Plata":
+				case "Cataratas":
+					aumento = 1.10;
+					break;
+				case "Cordoba":
+					aumento = 1;
+			}
+			break;
+	}
+
+	tarifaViajes = estadiaBase * aumento;
+
+	alert("El precio del viaje que quiere realizar es de: "+ tarifaViajes);
 }
